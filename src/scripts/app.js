8c303907d2df5948d23a7a985d8eae96fea5e955
mailysmae <79702008+mailysmae@users.noreply.github.com>
2022-08-26 18:17:18 +0200

    FEAT: add pic, add css

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,10 @@
-console.info('Hello world');
+"User strict"
+
+const box = document.querySelector('.box');
+let scrollLine =  document.querySelector('.scroll');
+
+box.addEventListener('wheel', (e)=> {
+      e.preventDefault();
+      box.scrollLeft += e.deltaY;
+      scrollLinr.style.width = box.scrollLeft / 2 + 'px';
+})
