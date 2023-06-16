775c84e485c8d390e752fdf5f4b616d59e79b1be
mailysmae <79702008+mailysmae@users.noreply.github.com>
2023-06-16 13:02:09 +0200

    FEAT: new version

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,3 @@
 "User strict"
 
-const box = document.querySelector('.box');
-let scrollLine =  document.querySelector('.scroll');
-
-box.addEventListener('wheel', (e)=> {
-      e.preventDefault();
-      box.scrollLeft += e.deltaY;
-      scrollLinr.style.width = box.scrollLeft / 2 + 'px';
-})
+console.log ('hello')
